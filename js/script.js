d312604b60ca7f07c76cad8b609c3817c922016d
Nermolart <nereamendez.alvarez@gmail.com>
2024-11-24 23:36:05 +0100

    update
    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function () {
 //menu bars
 const menuIcon = document.querySelector(".menu-icon");
@@ -44,19 +43,4 @@ fullscreenContainer.addEventListener('click', () => {
 const yearElement = document.getElementById("year");
     const currentYear = new Date().getFullYear(); // Obtiene el año actual
     yearElement.textContent = currentYear; // Establece el texto del año
-=======
-const ampliableImg = document.querySelector('.ampliable');
-const fullscreenContainer = document.getElementById('fullscreen-container');
-const fullscreenImg = document.getElementById('fullscreen-img');
-
-// Ampliar la imagen
-ampliableImg.addEventListener('click', () => {
-    fullscreenImg.src = ampliableImg.src; // Copia la fuente de la imagen
-    fullscreenContainer.style.display = 'flex'; // Muestra el contenedor
-});
-
-// Cerrar la imagen al hacer clic fuera de ella
-fullscreenContainer.addEventListener('click', () => {
-    fullscreenContainer.style.display = 'none'; // Oculta el contenedor
->>>>>>> 391eb95361774bff4526d0ecf59ee67c20394038
 });

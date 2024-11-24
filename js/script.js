af58029c8c2084807506e06ae69b02e1fb6fe877
Nermolart <nereamendez.alvarez@gmail.com>
2024-11-24 23:28:13 +0100

    update
    Web responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+//menu bars
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".close-icon");
+const menu = document.querySelector(".menu");
+
+// Abrir el menú
+menuIcon.addEventListener("click", function () {
+    menu.classList.add("show");
+});
+
+// Cerrar el menú con la "X"
+closeIcon.addEventListener("click", function () {
+    menu.classList.remove("show");
+});
+
+// Cerrar el menú si haces clic fuera de él
+document.addEventListener("click", function (e) {
+    if (!e.target.closest(".menu") && !e.target.closest(".menu-icon")) {
+        menu.classList.remove("show");
+    }
+});
+
+
+//CV
 const ampliableImg = document.querySelector('.ampliable');
 const fullscreenContainer = document.getElementById('fullscreen-container');
 const fullscreenImg = document.getElementById('fullscreen-img');
@@ -11,4 +36,11 @@ ampliableImg.addEventListener('click', () => {
 // Cerrar la imagen al hacer clic fuera de ella
 fullscreenContainer.addEventListener('click', () => {
     fullscreenContainer.style.display = 'none'; // Oculta el contenedor
+});
+
+
+//FECHA FOOTER
+const yearElement = document.getElementById("year");
+    const currentYear = new Date().getFullYear(); // Obtiene el año actual
+    yearElement.textContent = currentYear; // Establece el texto del año
 });
